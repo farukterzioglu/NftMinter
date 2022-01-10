56949a8f4dae3080b20a90ab21f110fc88058823
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2022-01-10 21:56:45 +0300

    Upload folder to ipfs

--- scripts/upload.ts
+++ scripts/upload.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import { strictEqual } from 'assert';
 import { CID, create, globSource, Options } from 'ipfs-http-client';
+import { string } from 'hardhat/internal/core/params/argumentTypes';
 
 const globSourceOptions = {
   hidden: false
@@ -14,26 +15,32 @@ const client = create( {
   }
 })
 
-async function uploadFolderToIpfs(folderPath :string) : Promise<string> {
+class UploadDetails
+{
+  FolderCid : string;
+  FileNames: string[];
+
+  constructor(){
+    this.FileNames = new Array<string>()
+    this.FolderCid = "";
+  }
+}
+async function uploadFolderToIpfs(folderPath :string) : Promise<UploadDetails> {
+  const uploadDetails = new UploadDetails();
+
   const addOptions = {
     pin: true,
     wrapWithDirectory: true,
     timeout: 60000
   };
 
-  let cid: CID;
   for await (const file of client.addAll(globSource(folderPath, "**/*", globSourceOptions), addOptions)) {
-    cid = file.cid;
-  }
+    uploadDetails.FileNames.push(file.path);
+    uploadDetails.FolderCid = file.cid.toString();
+  }   
+  uploadDetails.FileNames.splice(uploadDetails.FileNames.length -1);
 
-  return cid!.toString();
-}
-async function uploadImageToIpfs(filePath :string) : Promise<string> {
-  const file = fs.readFileSync(filePath);
-
-  const added = await client.add(file);
-  const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-  return url;
+  return uploadDetails;
 }
 
 async function uploadMetadataToIpfs(imagePath:string, name:string, desc:string) : Promise<string> {
@@ -49,37 +56,39 @@ async function uploadMetadataToIpfs(imagePath:string, name:string, desc:string) 
   return metadataUrl;
 }
 
-async function uploadNftImages(nftImagesPath: string, baseUrl: string) : Promise<string[]> {
-  const uploadedPathCID = await uploadFolderToIpfs(`${nftImagesPath}`);
+class FileNameMetadataPair
+{
+  fileName :string;
+  metaDataUri: string;
 
-  return [];
+  constructor(fileName :string,metaDataUri: string){
+    this.fileName = fileName;
+    this.metaDataUri = metaDataUri;
+  }
 
-  // Read images
-  const ntfImageList = new Array<string>();
-  fs.readdirSync(nftImagesPath).forEach(file => {
-    ntfImageList.push(file);
-  });
+}
+async function uploadNftImages(nftImagesPath: string, baseUrl: string) : Promise<FileNameMetadataPair[]> {
+  const fileNameMetadataPairs = new Array<FileNameMetadataPair>();
 
-  // Upload images
-  const ntfImageUrlList = new Array<string>();
-  for (let i = 0; i < ntfImageList.length; i++) {
-    const file = ntfImageList[i];
-
-    const uploadedPath = await uploadImageToIpfs(`${nftImagesPath}/${file}`);
-    const metadataUrl = await uploadMetadataToIpfs(uploadedPath, file, file);
-
-    ntfImageUrlList.push(metadataUrl);
-  };
-
-  return ntfImageUrlList;
+  const uploadDetails = await uploadFolderToIpfs(`${nftImagesPath}`);
+  for (let i = 0; i < uploadDetails.FileNames.length; i++) {
+    const filename = uploadDetails.FileNames[i];
+    const fileUri = `https://ipfs.infura.io/ipfs/${uploadDetails.FolderCid}/${filename}`;
+    
+    const uri = await uploadMetadataToIpfs(fileUri, filename, filename);
+    fileNameMetadataPairs.push(new FileNameMetadataPair(filename, uri));
+  }
+  return fileNameMetadataPairs;
 }
 
 // hh run scripts/upload.ts
 async function main() {
   const baseUrl = "";
 
-  const ntfImageList = await uploadNftImages("files/nftimages", baseUrl);
-  ntfImageList.forEach(nftImagesPath => { console.log(`${nftImagesPath}`); });
+  const fileNameMetadataPairs = await uploadNftImages("files/nftimages", baseUrl);
+  fileNameMetadataPairs.forEach(fileNameMetadataPair => { 
+    console.log(`${fileNameMetadataPair.fileName} \t ${fileNameMetadataPair.metaDataUri}`); 
+  });
 
   // TODO: Create NFT and mint for the list
 
@@ -91,3 +100,34 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// Notes 
+// async function uploadImageToIpfs(filePath :string) : Promise<string> {
+//   const file = fs.readFileSync(filePath);
+
+//   const added = await client.add(file);
+//   const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+//   return url;
+// }
+
+// // old code
+// async function readFilesAndupload(nftImagesPath: string) : Promise<string[]>{
+//   // Read images
+//   const ntfImageList = new Array<string>();
+//   fs.readdirSync(nftImagesPath).forEach(file => {
+//     ntfImageList.push(file);
+//   });
+
+//   // Upload images
+//   const ntfImageUrlList = new Array<string>();
+//   for (let i = 0; i < ntfImageList.length; i++) {
+//     const file = ntfImageList[i];
+
+//     const uploadedPath = await uploadImageToIpfs(`${nftImagesPath}/${file}`);
+//     const metadataUrl = await uploadMetadataToIpfs(uploadedPath, file, file);
+
+//     ntfImageUrlList.push(metadataUrl);
+//   };
+
+//   return ntfImageUrlList;
+// }
