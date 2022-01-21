0c715e73addf612b24ae573910d4af50a4bb5f23
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2022-01-22 01:19:34 +0300

    Refactor ipfs imp.

--- scripts/upload.ts
+++ scripts/upload.ts
@@ -4,19 +4,19 @@ import { CID, create, globSource, Options } from 'ipfs-http-client';
 import { string } from 'hardhat/internal/core/params/argumentTypes';
 import { ethers } from "hardhat";
 
-const globSourceOptions = {
-  hidden: false
-};
-
 const auth = 'Basic ' + Buffer.from(process.env.IPFS_PROJECTID + ':' + process.env.IPFS_SECRET).toString('base64')
-const client = create( { 
+const remoteClient = create( { 
   url: "https://ipfs.infura.io:5001/api/v0",
   headers: {
     authorization: auth
   }
 })
 
-class UploadDetails
+const ipfsClient = create( { 
+  url: "http://127.0.0.1:5001"
+})
+
+class FileUploadResponse
 {
   FolderCid : string;
   FileNames: string[];
@@ -26,16 +26,16 @@ class UploadDetails
     this.FolderCid = "";
   }
 }
-async function uploadFolderToIpfs(folderPath :string) : Promise<UploadDetails> {
-  const uploadDetails = new UploadDetails();
-
+async function uploadFolderToIpfs(folderPath :string) : Promise<FileUploadResponse> {
   const addOptions = {
     pin: true,
     wrapWithDirectory: true,
     timeout: 60000
   };
+  const globSourceOptions = { hidden: false };
 
-  for await (const file of client.addAll(globSource(folderPath, "**/*", globSourceOptions), addOptions)) {
+  const uploadDetails = new FileUploadResponse();
+  for await (const file of ipfsClient.addAll(globSource(folderPath, "**/*", globSourceOptions), addOptions)) {
     console.log( {image: file});
     uploadDetails.FileNames.push(file.path);
     uploadDetails.FolderCid = file.cid.toString();
@@ -60,30 +60,20 @@ class Metadata
   }
 }
 
-class MetadataPair {
-  CID : string;
-  Path : string;
-
-  constructor(cid: string, path: string) {
-    this.CID = cid;
-    this.Path = path;
-  }
-}
-class MetadataUploadDetails
+class MetadataUploadResponse
 {
   FolderCid : string;
-  MetadataFileNameUriPairs: MetadataPair[];
+  FileNames: string[];
 
   constructor(){
-    this.MetadataFileNameUriPairs = new Array<MetadataPair>();
+    this.FileNames = new Array<string>();
     this.FolderCid = "";
   }
 }
 
-async function uploadMetadataListToIpfs(metadataList : Metadata[]) : Promise<MetadataUploadDetails> {
+async function uploadMetadataListToIpfs(metadataList : Metadata[]) : Promise<MetadataUploadResponse> {
   console.log( { metadataList: metadataList });
   let jsonList : Array<string> = metadataList.map( metadata => JSON.stringify(metadata));
-  console.log( { metadataJsonList: jsonList} );
 
   const addOptions = {
     pin: true,
@@ -92,73 +82,62 @@ async function uploadMetadataListToIpfs(metadataList : Metadata[]) : Promise<Met
   };
 
   // TODO: Upload with file name (e.g. `baseuri/1.json` )
-  const metadataUploadDetails = new MetadataUploadDetails();
+  const metadataUploadResponse = new MetadataUploadResponse();
   let i: number = 0;
-  for await (const file of client.addAll(jsonList, addOptions)) {
+  for await (const file of ipfsClient.addAll(jsonList, addOptions)) {
     // Last item is the folder CID
     if(i == (metadataList.length) )
     {
-      metadataUploadDetails.FolderCid = file.cid.toString();
+      metadataUploadResponse.FolderCid = file.cid.toString();
     }
     else 
     {
-      const pair = new MetadataPair(file.cid.toString(), file.path);
-      console.log(pair);
-      metadataUploadDetails.MetadataFileNameUriPairs.push(pair);
+      metadataUploadResponse.FileNames.push(file.cid.toString());
       i++;
     }
-  }   
-  // metadataUploadDetails.MetadataFileNameUriPairs.splice(metadataUploadDetails.MetadataFileNameUriPairs.length -1);
-  console.log({metadataUploadDetails:metadataUploadDetails});
+  }
+  console.log({metadataUploadDetails:metadataUploadResponse});
 
-  return metadataUploadDetails;
-}
-
-async function uploadMetadataToIpfs(imagePath:string, name:string, desc:string) : Promise<string> {
-  const metadata = {
-    "description": desc,
-    "image": imagePath,
-    "name": name
-  };
-
-  const addedMetadata = await client.add(JSON.stringify(metadata));
-  const metadataUrl = `https://ipfs.infura.io/ipfs/${addedMetadata.path}`;
-
-  return metadataUrl;
+  return metadataUploadResponse;
 }
 
 class FileNameMetadataPair
 {
   fileName :string;
   metaDataUri: string;
+  metaDataPublicUri: string;
 
-  constructor(fileName :string,metaDataUri: string){
+  constructor(fileName :string,metaDataUri: string, metaDataPublicUri: string = ""){
     this.fileName = fileName;
     this.metaDataUri = metaDataUri;
+    this.metaDataPublicUri = metaDataPublicUri;
   }
 
 }
 async function uploadNftImages(nftImagesPath: string, baseUrl: string) : Promise<FileNameMetadataPair[]> {
   // Upload all images under a folder 
-  const uploadDetails :UploadDetails = await uploadFolderToIpfs(`${nftImagesPath}`);
+  const uploadDetails :FileUploadResponse = await uploadFolderToIpfs(nftImagesPath);
 
   // Create metadata for uploaded images
   let metadaList : Array<Metadata> = uploadDetails.FileNames.map( fn => 
-    new Metadata(`https://ipfs.infura.io/ipfs/${uploadDetails.FolderCid}/${fn}`, fn, `A ${fn} NFT`));
+    new Metadata(`ipfs://${uploadDetails.FolderCid}/${fn}`, fn, `A ${fn} NFT`));
 
   // Upload metadata list to a IPFS folder
-  const metadataUploadDetails: MetadataUploadDetails = await uploadMetadataListToIpfs(metadaList);
+  const metadataUploadDetails: MetadataUploadResponse = await uploadMetadataListToIpfs(metadaList);
 
   // Create filename-metadataUri pairs
   const fileNameMetadataPairs = new Array<FileNameMetadataPair>();
-  for (let i = 0; i < metadataUploadDetails.MetadataFileNameUriPairs.length; i++) {
-    const fileNameUriPair = metadataUploadDetails.MetadataFileNameUriPairs[i];
-    const fileUri = `https://ipfs.infura.io/ipfs/${metadataUploadDetails.FolderCid}/${fileNameUriPair.CID}`;
+  for (let i = 0; i < metadataUploadDetails.FileNames.length; i++) {
+    const fileName = metadataUploadDetails.FileNames[i];
+    // const metaDataUri = `https://ipfs.infura.io/ipfs/${metadataUploadDetails.FolderCid}/${fileNameUriPair}`;
+    const metaDataUri = `ipfs://${metadataUploadDetails.FolderCid}/${fileName}`;
 
-    fileNameMetadataPairs.push(new FileNameMetadataPair(uploadDetails.FileNames[i], fileUri));
+    remoteClient.pin.add(`${metadataUploadDetails.FolderCid}/${fileName}`);
+    const publicUri = `https://ipfs.infura.io/ipfs/${metadataUploadDetails.FolderCid}/${fileName}`;
+
+    fileNameMetadataPairs.push(new FileNameMetadataPair(uploadDetails.FileNames[i], metaDataUri, publicUri));
   }
   console.log({fileNameMetadataPairs: fileNameMetadataPairs});
-
   return fileNameMetadataPairs;
 }
 
@@ -167,9 +146,7 @@ async function main() {
   const baseUrl = "";
 
   const fileNameMetadataPairs = await uploadNftImages("files/nftimages", baseUrl);
-  fileNameMetadataPairs.forEach(fileNameMetadataPair => { 
-    console.log(`${fileNameMetadataPair.fileName} \t ${fileNameMetadataPair.metaDataUri}`); 
-  });
+  // fileNameMetadataPairs.forEach(fileNameMetadataPair => { console.log(`${fileNameMetadataPair.fileName} \t ${fileNameMetadataPair.metaDataUri}`); });
 
   const GenericNft = await ethers.getContractFactory("GenericNft");
   const genericNft = await GenericNft.deploy("MatrixPosters", "MTRX", "");
@@ -194,6 +171,55 @@ main().catch((error) => {
 });
 
 // Notes 
+
+// async function uploadFolderToIpfs1(nftImagesPath: string) : Promise<string[]>{
+//   // Read images
+//   const ntfImageList = new Array<string>();
+//   fs.readdirSync(nftImagesPath).forEach(file => {
+//     ntfImageList.push(file);
+//   });
+
+//   // Upload images
+//   const ntfImageUrlList = new Array<string>();
+//   for (let i = 0; i < ntfImageList.length; i++) {
+//     const filePath = ntfImageList[i];
+//     const file = fs.readFileSync(filePath);
+
+//     const ipfsPath = '/nft/' + file
+//     // TODO:
+//     // const { cid: assetCid } = await client.add({ path: ipfsPath, file })
+//     // ntfImageUrlList.push(metadataUrl);
+//   };
+
+//   return ntfImageUrlList;
+// }
+
+// function uploadWithFileName()
+// {
+//   for (let i = 0; i < metadataList.length; i++) {
+//     const metadata = metadataList[i];
+
+//     const { cid: metadataCid } = await client.add({ 
+//       path: `/nft/${metadata.name}.json`, 
+//       content: JSON.stringify(metadata)
+//     })
+//     console.log({ metadataCid: `${metadataCid}/${metadata.name}.json`});
+    
+//   }
+// }
+// async function uploadMetadataToIpfs(imagePath:string, name:string, desc:string) : Promise<string> {
+//   const metadata = {
+//     "description": desc,
+//     "image": imagePath,
+//     "name": name
+//   };
+
+//   const addedMetadata = await client.add(JSON.stringify(metadata));
+//   const metadataUrl = `https://ipfs.infura.io/ipfs/${addedMetadata.path}`;
+
+//   return metadataUrl;
+// }
+
 // async function uploadImageToIpfs(filePath :string) : Promise<string> {
 //   const file = fs.readFileSync(filePath);
 
